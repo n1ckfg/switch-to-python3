01231002814957ad12eb973e8456b0891d305456
n1ckfg <nick@fox-gieg.com>
2021-11-15 10:17:25 -0500

    shell

--- ubuntu/reset-python.sh
+++ ubuntu/reset-python.sh
@@ -1,4 +1,13 @@
+PYTHON_VERSION=3.8
+
+sudo apt-get remove python$PYTHON_VERSION
+sudo apt-get autoremove python$PYTHON_VERSION
+sudo apt-get purge python$PYTHON_VERSION
+
 sudo update-alternatives --remove-all python
-sudo ln -sf /usr/bin/python2.7 /usr/bin/python
-sudo ln -fg /ur/bin/python3.6 /usr/bin/python3
+sudo update-alternatives --remove-all python3
+
+sudo apt-get install --reinstall python
+sudo apt-get install --reinstall python3
+
 
