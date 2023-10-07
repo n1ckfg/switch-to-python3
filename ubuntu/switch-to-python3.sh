754907b7e3114ab20aaed5b6e523b20094034d05
Nick Fox-Gieg <nick@fox-gieg.com>
2023-10-07 18:41:28 -0400

    Update switch-to-python3.sh

--- ubuntu/switch-to-python3.sh
+++ ubuntu/switch-to-python3.sh
@@ -1,7 +1,7 @@
 # https://unix.stackexchange.com/questions/410579/change-the-python3-default-version-in-ubuntu
 # https://linuxize.com/post/how-to-install-python-3-7-on-ubuntu-18-04/
 
-PYTHON_VERSION=3.8
+PYTHON_VERSION=3.10
 
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
@@ -16,6 +16,9 @@ sudo rm /usr/bin/python
 sudo rm /usr/bin/python3
 sudo ln -sf /usr/bin/python$PYTHON_VERSION /usr/bin/python
 sudo ln -sf /usr/bin/python$PYTHON_VERSION /usr/bin/python3
+
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
 
 python -V
 pip -V
